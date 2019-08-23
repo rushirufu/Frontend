3f4650ff75399eea2a2e711a9a4e6b0b231628b0
Moises Aguiar <aguiar_moises@hotmail.com>
2019-08-23 09:13:33 -0400

    asdasd

---
+++ src/RactJS/src/LearningReact/solution/src/componentes/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Footer extends React.Component{
+
+  render(){
+    <div>
+      <h1>Esto es un footer</h1>
+      <p>bla bla bla bla bla bla bla bla bla bla</p>
+    </div>
+  }
+}
