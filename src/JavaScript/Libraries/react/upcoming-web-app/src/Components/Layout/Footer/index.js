9c7b2de9ed9cea050c0b26834133c727fe3bcb92
Moises Aguiar <aguiar_moises@hotmail.com>
2019-08-28 17:04:49 -0400

    trabajando con react

---
+++ src/JavaScript/Libraries/react/upcoming-web-app/src/Components/Layout/Footer/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './index.css';
+
+function index(){
+    return (
+        <footer className="ClassHere">
+        <p>Copyright© by Company Name 2019</p>
+        </footer> 
+    );
+}
+export default index;
---
+++ src/JavaScript/Libraries/react/upcoming-web-app/src/Components/Views/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import Button from '@material-ui/core/Button';
+
+function Index(){
+    return (
+        <div className="ClassHere">
+            <Button variant="contained" color="primary">
+            Hello World
+            </Button>
+        </div>
+      );
+}
+export default Index;
